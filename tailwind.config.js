029395cdcf8fa23665055c2a9d99d3aeec06194c
devdasz <dasprasun1234@gmail.com>
2023-06-22 18:23:53 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      "colors": {
+        "primary": {
+          "50": "#ebf4f1",
+          "100": "#d8e8e2",
+          "200": "#b1d1c5",
+          "300": "#89baa8",
+          "400": "#62a38b",
+          "500": "#3b8c6e",
+          "600": "#2f7058",
+          "700": "#235442",
+          "800": "#18382c",
+          "900": "#0c1c16"
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
